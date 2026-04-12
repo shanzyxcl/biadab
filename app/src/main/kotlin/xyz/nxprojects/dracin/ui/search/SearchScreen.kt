613f8a50b674fef35aec4dbafb2470fee8c05ed4
shanzyxcl <161633828+shanzyxcl@users.noreply.github.com>
2026-04-12 17:55:48 +0700

    Add files via upload

--- app/src/main/kotlin/xyz/nxprojects/dracin/ui/search/SearchScreen.kt
+++ app/src/main/kotlin/xyz/nxprojects/dracin/ui/search/SearchScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
