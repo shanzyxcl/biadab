bf36f9ca2ee7e89021845a8a9bbcc2a390b59fd1
shanzyxcl <161633828+shanzyxcl@users.noreply.github.com>
2026-04-27 23:54:31 +0700

    Add files via upload

--- app/src/main/kotlin/xyz/nxprojects/dracin/MainActivity.kt
+++ app/src/main/kotlin/xyz/nxprojects/dracin/MainActivity.kt
@@ -8,8 +8,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 import xyz.nxprojects.dracin.ui.NxApp
 
@@ -18,8 +21,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         
         setContent {
+            val systemUiController = rememberSystemUiController()
             val darkColorScheme = darkColorScheme(
                 primary = Color(0xFFF43F5E),
                 secondary = Color(0xFFEAB308),
@@ -29,6 +34,17 @@ class MainActivity : ComponentActivity() {
                 onBackground = Color.White,
                 onSurface = Color.White
             )
+
+            SideEffect {
+                systemUiController.setStatusBarColor(
+                    color = Color.Transparent,
+                    darkIcons = false
+                )
+                systemUiController.setNavigationBarColor(
+                    color = Color.Transparent,
+                    darkIcons = false
+                )
+            }
 
             MaterialTheme(
                 colorScheme = darkColorScheme
